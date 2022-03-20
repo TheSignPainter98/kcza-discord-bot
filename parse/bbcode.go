1d0e991d25e8a63624c2f89b2a34c06c361b5b13
Ed Jones <edjones98@gmail.com>
2022-03-20 20:21:21 +0000

    Added list handling, improved tag handling

--- parse/bbcode.go
+++ parse/bbcode.go
@@ -1,3 +1,4 @@
+// NOTE: BBCode parsers exist for go, I just wanted to write one myself for my own interest
 package parse
 
 import (
@@ -16,26 +17,37 @@ type BBCode struct {
 }
 
 type BBElem struct {
-	Word *string `  @Word`
+	List *BBList `  @@`
 	Tag  *BBTag  `| @@`
+	Word *string `| (@Word | @Assign)`
+}
+
+type BBList struct {
+	Type  *string   `"[list" ("=" @Word)? "]"`
+	Items []*BBCode `( "[*]" @@ )* "[/" "list" "]"`
 }
 
 type BBTag struct {
-	Name *string `"[" @Word "]"`
-	// Attrs     []*BBAttr `@@* "]"`
-	Body      *BBCode `@@`
-	CloseName *string `"[/" @Word "]"`
+	Name     *string `"[" @Word`
+	Attrs    BBAttrs `@@ "]"`
+	Body     *BBCode `@@`
+	CloseTag *string `"[/" @Word "]"`
 }
 
-// type BBAttr struct {
-// 	Key   *string `@Word "="`
-// 	Value *string `@Word`
-// }
+type BBAttrs struct {
+	RawAttrs []*BBAttr `@@*`
+	Attrs    map[string]string
+}
+
+type BBAttr struct {
+	Key   *string `@Word "="`
+	Value *string `@Word`
+}
 
 func BBCodeToMd(bbcRaw string) (string, error) {
 	bbc, err := parseBBCode(bbcRaw)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 	builder := new(strings.Builder)
 	bbc.ToMd(builder)
@@ -46,13 +58,14 @@ func parseBBCode(bbcRaw string) (*BBCode, error) {
 	bbcodeLexer := lexer.MustSimple([]lexer.Rule{
 		{"whitespace", `[\s\r\n]+`, nil}, // (Auto-ignores white-space)
 
-		{"OpenStartTag", `\[`, nil},
+		{"ListItem", `\[\*\]`, nil},
+		{"OpenListTag", `\[list`, nil},
 		{"OpenEndTag", `\[/`, nil},
-		{"CloseTag", `]`, nil},
-		// {"Assign", `=`, nil},
-		// {"OpenTag", `\[[^\]]+\]`, nil},
-		// {"CloseTag", `\[/[^\]]+\]`, nil},
-		{"Word", `[^\s[\]]+`, nil},
+		{"OpenStartTag", `\[`, nil},
+		{"CloseTag", `\]`, nil},
+		{"Assign", `=`, nil},
+
+		{"Word", `[^\s\[=\]]+`, nil},
 	})
 	bbcodeParser := participle.MustBuild(&BBCode{}, participle.Lexer(bbcodeLexer))
 
@@ -77,10 +90,28 @@ func (c *BBCode) ToMd(b *strings.Builder) {
 func (e *BBElem) ToMd(b *strings.Builder) {
 	if e.Word != nil {
 		b.WriteString(*e.Word)
-	} else {
+	} else if e.Tag != nil {
 		e.Tag.ToMd(b)
+	} else {
+		e.List.ToMd(b)
 	}
 }
+
+func (l *BBList) ToMd(b *strings.Builder) {
+	var listMarker string
+	if l.Type != nil && *l.Type == "1" {
+		listMarker = "1. "
+	} else {
+		listMarker = "- "
+	}
+	for _, e := range l.Items {
+		b.WriteString(listMarker)
+		e.ToMd(b)
+		b.WriteString("\n")
+	}
+}
+
+type complicatedMdifierFunc func(*BBTag, *strings.Builder)
 
 func (t *BBTag) ToMd(b *strings.Builder) {
 	simpleNameMap := map[string]string{
@@ -88,25 +119,36 @@ func (t *BBTag) ToMd(b *strings.Builder) {
 		"i":    "_",
 		"code": "`",
 	}
-	if n, present := simpleNameMap[*t.Name]; present {
+	complicatedNameMap := map[string]complicatedMdifierFunc{
+		"link": makeLink,
+	}
+	if f, present := complicatedNameMap[*t.Name]; present {
+		t.Attrs.Attrs = make(map[string]string)
+		for _, a := range t.Attrs.RawAttrs {
+			t.Attrs.Attrs[*a.Key] = *a.Value
+		}
+		f(t, b)
+	} else if n, present := simpleNameMap[*t.Name]; present {
 		b.WriteString(n)
 		t.Body.ToMd(b)
 		b.WriteString(n)
 	} else {
 		t.Body.ToMd(b)
 	}
-	// // This would be nicer using an interface to implement a type union
-	// if t.Bold != nil {
-	//	b.WriteString("**")
-	//	t.Bold.ToMd(b)
-	//	b.WriteString("**")
-	// } else if t.Italic != nil {
-	//	b.WriteString("_")
-	//	t.Italic.ToMd(b)
-	//	b.WriteString("_")
-	// } else if t.Code != nil {
-	//	b.WriteString("`")
-	//	t.Code.ToMd(b)
-	//	b.WriteString("`")
-	// }
+}
+
+func makeLink(t *BBTag, b *strings.Builder) {
+	if l, p := t.Attrs.Attrs["url"]; p {
+		b.WriteString("[")
+		t.Body.ToMd(b)
+		b.WriteString("](")
+		b.WriteString(l)
+		b.WriteString(")")
+	} else {
+		b.WriteString("[")
+		t.Body.ToMd(b)
+		b.WriteString("](")
+		t.Body.ToMd(b)
+		b.WriteString(")")
+	}
 }
--- parse/bbcode_test.go
+++ parse/bbcode_test.go
@@ -1,7 +1,9 @@
 package parse
 
 import (
+	"fmt"
 	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -16,34 +18,111 @@ func TestBBCodeSimple(t *testing.T) {
 	}
 }
 
-func TestBBCodeBold(t *testing.T) {
-	raw := "[b]something bold[/b]"
-	bbc, err := BBCodeToMd(raw)
-	if err != nil && bbc != "" {
+func tagTest(t *testing.T, tag, mdTag, mdPat string) {
+	testText := "something to test"
+	raw := fmt.Sprintf("[%s]%s[/%s]", tag, testText, tag)
+	md, err := BBCodeToMd(raw)
+	if err != nil && md != "" {
 		t.Fatalf("Expected either non-nil error or non-empty string and non-empty input")
 	}
 
-	exp := `\*\*something bold\*\*`
-	want := regexp.MustCompile(exp)
 	if err == nil {
-		if !want.MatchString(bbc) {
-			t.Fatalf("Output does not contain input: %#v doesn't contain %#v", bbc, exp)
+		want := regexp.MustCompile(mdPat + testText + mdPat)
+		if !want.MatchString(md) {
+			exp := mdTag + testText + mdTag
+			t.Fatalf("Incorrect output: expected %#v but got %#v", exp, md)
 		}
 	}
 }
 
+func TestBBCodeBold(t *testing.T) {
+	tagTest(t, "b", "**", `\*\*`)
+}
+
 func TestBBCodeItalic(t *testing.T) {
-	raw := "[i]something bold[/i]"
-	bbc, err := BBCodeToMd(raw)
-	if err != nil && bbc != "" {
+	tagTest(t, "i", "_", "_")
+}
+
+func TestBBCodeCode(t *testing.T) {
+	tagTest(t, "code", "`", "`")
+}
+
+func TestLinkSimple(t *testing.T) {
+	testLink := "https://kcza.net"
+	raw := fmt.Sprintf("[link]%s[/link]", testLink)
+	md, err := BBCodeToMd(raw)
+	if err != nil && md != "" {
 		t.Fatalf("Expected either non-nil error or non-empty string and non-empty input")
 	}
 
-	exp := `^_something bold_$`
-	want := regexp.MustCompile(exp)
 	if err == nil {
-		if !want.MatchString(bbc) {
-			t.Fatalf("Output does not contain input: %#v doesn't contain %#v", bbc, exp)
+		want := regexp.MustCompile(fmt.Sprintf(`\[%s\]\(%s\)`, testLink, testLink))
+		if !want.MatchString(md) {
+			t.Fatalf("Incorrect output: expected %#v but got %#v", want, md)
 		}
+	}
+}
+
+func TestLinkWithUrl(t *testing.T) {
+	testLink := "https://kcza.net"
+	testText := "some text to test"
+	raw := fmt.Sprintf("[link url=%s]%s[/link]", testLink, testText)
+	md, err := BBCodeToMd(raw)
+	if err != nil && md != "" {
+		t.Fatalf("Expected either non-nil error or non-empty string and non-empty input")
+	}
+
+	if err == nil {
+		want := regexp.MustCompile(fmt.Sprintf(`\[%s\]\(%s\)`, testText, testLink))
+		if !want.MatchString(md) {
+			t.Fatalf("Incorrect output: expected %#v but got %#v", want, md)
+		}
+	}
+}
+
+func TestListNumeric(t *testing.T) {
+	testListType(t, "1", `[0-9]+\.`)
+}
+
+func TestListBullet(t *testing.T) {
+	testListType(t, "", `[-*]`)
+}
+
+func TestListUnsupported(t *testing.T) {
+	unsupportedPat := `([0-9]+\.|[-*])`
+	testListType(t, "a", unsupportedPat)
+	testListType(t, "A", unsupportedPat)
+	testListType(t, "i", unsupportedPat)
+	testListType(t, "I", unsupportedPat)
+}
+
+func testListType(t *testing.T, listType, mdItemPat string) {
+	hdrText := ""
+	if len(listType) > 0 {
+		hdrText = "=" + listType
+	}
+	// lines := []string{"Hello", "World!", "How", "are", "you?", "", ""}
+	lines := []string{"Hello", "World!", "How", "are", "you?"}
+	bbcListBody := make([]string, len(lines))
+	for i, l := range lines {
+		bbcListBody[i] = "[*]" + l
+	}
+	raw := fmt.Sprintf("[list%s]%s[/list]", hdrText, strings.Join(bbcListBody, ""))
+	md, err := BBCodeToMd(raw)
+	if err != nil && md != "" {
+		t.Fatalf("Expected either non-nil error or non-empty string and non-empty input")
+	}
+
+	if err == nil {
+		mdListBody := make([]string, len(lines))
+		for i, l := range lines {
+			mdListBody[i] = mdItemPat + " " + l
+		}
+		want := regexp.MustCompile(strings.Join(mdListBody, "\n"))
+		if !want.MatchString(md) {
+			t.Fatalf("Incorrect output: expected %#v but got %#v", want, md)
+		}
+	} else {
+		t.Fatalf("Should have been able to parse %#v, got %#v: %s", raw, md, err)
 	}
 }
