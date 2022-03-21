6d04837469d3aaf591455b78189665fb730c6043
Ed Jones <edjones98@gmail.com>
2022-03-21 21:56:47 +0000

    Refactored web-watcher generation for better modularity

--- bot/geek-hack.go
+++ bot/geek-hack.go
@@ -1,70 +1,35 @@
 package bot
 
 import (
-	"errors"
 	"fmt"
-	"golang-discord-bot/actions"
-	"golang-discord-bot/parse"
-	"net/http"
-	"strings"
+	"regexp"
 	"time"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 type TopicID int
 
 type GeekHackWatcher struct {
-	Name string
-	Url  string
+	WebWatcher
 	TopicID
-	outputChannel string
+}
+
+func init() {
+	webWatcherConstructors = append(webWatcherConstructors, TryNewGeekHackWatcher)
 }
 
 const retryBackoff = 1 * time.Minute
 
-func (g GeekHackWatcher) Watch(act chan actions.DiscordAction) {
-	fmt.Println("Hello, geekhack " + g.Name)
-	for true {
-		fmt.Printf("Sleeping on %#v\n", g.Name)
-		pageText, err := g.GetPageText()
-		if err != nil {
-			fmt.Println(err)
-			time.Sleep(retryBackoff)
-			continue
-		}
-
-		bbc, err := parse.BBCodeToMd(pageText)
-		if err != nil {
-			fmt.Println(err)
-			time.Sleep(retryBackoff)
-			continue
-		}
-
-		act <- actions.MessageAction{g.outputChannel, fmt.Sprintf("Geekhack update for %v!", g.Name)}
-		act <- actions.MessageAction{g.outputChannel, "This is another quite quick message"}
-		act <- actions.LongMessage(g.outputChannel, bbc)
-		time.Sleep(5 * time.Minute)
-	}
-}
-
-func (g GeekHackWatcher) GetPageText() (string, error) {
-	resp, err := http.Get(g.Url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return "", errors.New(fmt.Sprintf("Returned status code %d\n", resp.StatusCode))
+func TryNewGeekHackWatcher(name, url, outputChannel string) Watcher {
+	if !regexp.MustCompile(`geekhack\.org/index\.php`).MatchString(url) {
+		return nil
 	}
 
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		return "", err
+	rawTopic := regexp.MustCompile(`topic=\d+`).FindString(url)
+	if len(rawTopic) == 0 {
+		return nil
 	}
+	var topic TopicID
+	fmt.Sscanf(url, "topic=%d", &topic)
 
-	thing := doc.Find(".postarea").First()
-
-	return strings.TrimSpace(thing.Text()), nil
+	return &GeekHackWatcher{WebWatcher{name, url, outputChannel}, topic}
 }
--- bot/web-watch.go
+++ bot/web-watch.go
@@ -2,11 +2,22 @@ package bot
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"golang-discord-bot/actions"
 	"golang-discord-bot/cfg"
+	"golang-discord-bot/parse"
 	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
+
+type TryMakeWebWatcherFunc func(name, url, outputChannel string) Watcher
+
+var webWatcherConstructors []TryMakeWebWatcherFunc
 
 type Watcher interface {
 	Watch(chan actions.DiscordAction)
@@ -15,6 +26,12 @@ type Watcher interface {
 type WebWatcherSource struct {
 	Name string `json: "name"`
 	Url  string `json : "url"`
+}
+
+type WebWatcher struct {
+	Name          string
+	Url           string
+	OutputChannel string
 }
 
 func InitWebWatchers(config *cfg.Config, outputChannel string) ([]Watcher, error) {
@@ -45,6 +62,56 @@ func readWatchers(loc string) ([]WebWatcherSource, error) {
 }
 
 func (w WebWatcherSource) WebWatcher(outputChannel string) Watcher {
-	// TODO: parse urls, make this a factory
-	return GeekHackWatcher{w.Name, w.Url, 123, outputChannel}
+	for _, f := range webWatcherConstructors {
+		if o := f(w.Name, w.Url, outputChannel); o != nil {
+			return o
+		}
+	}
+	return nil
+}
+
+func (g WebWatcher) Watch(act chan actions.DiscordAction) {
+	for true {
+		fmt.Printf("Sleeping on %#v\n", g.Name)
+		pageText, err := g.GetPageText()
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(retryBackoff)
+			continue
+		}
+
+		bbc, err := parse.BBCodeToMd(pageText)
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(retryBackoff)
+			continue
+		}
+
+		act <- actions.MessageAction{g.OutputChannel, fmt.Sprintf("Geekhack update for %v!", g.Name)}
+		act <- actions.MessageAction{g.OutputChannel, "This is another quite quick message"}
+		act <- actions.LongMessage(g.OutputChannel, bbc)
+		time.Sleep(5 * time.Minute)
+	}
+}
+
+func (g WebWatcher) GetPageText() (string, error) {
+	fmt.Printf("Getting page text from %s\n", g.Url)
+	resp, err := http.Get(g.Url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", errors.New(fmt.Sprintf("Returned status code %d\n", resp.StatusCode))
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	thing := doc.Find(".postarea").First()
+
+	return strings.TrimSpace(thing.Text()), nil
 }
